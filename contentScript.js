b29f9cec7b59905c53d71aaaa9098d61fb468151
Chunmyong Park <chunmyong.park@supertone.ai>
2024-02-17 10:09:52 +0900

    Check popup.js working

--- contentScript.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "getHTML") {
-      const html = document.documentElement.outerHTML;
-      sendResponse({ html: html });
-    }
-  });
--- popup.js
+++ popup.js
@@ -1,16 +1,6 @@
-document.getElementById('crawlButton').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "getHTML" }, (response) => {
-        if (chrome.runtime.lastError) {
-          console.error(`Error: ${chrome.runtime.lastError.message}`);
-          return;
-        }
-  
-        if (response && response.html) {
-          console.log('HTML of the page:', response.html);
-          // 여기에 HTML 코드를 처리하는 로직을 추가하세요.
-        }
-      });
-    });
-  });
-  
+document.addEventListener('DOMContentLoaded', function () {
+    var checkButton = document.getElementById('crawlButton');
+    checkButton.addEventListener('click', function () {
+        document.getElementById('message').textContent = 'Success';
+    }, false);
+}, false);
