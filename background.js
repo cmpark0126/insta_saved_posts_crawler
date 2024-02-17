346ad4fc12895327d18bd1894dabb552d52a263f
Chunmyong Park <chunmyong.park@supertone.ai>
2024-02-17 13:51:15 +0900

    Refactor scroll algorithm

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       // contentScript로부터 비동기 작업 완료 메시지를 받으면 알림을 생성합니다.
       chrome.notifications.create('', {
         type: 'basic',
-        iconUrl: 'icon48.png',
+        iconUrl: 'images/icon48.png',
         title: 'Async Job Notification',
         message: message.result,
         priority: 2
--- contentScript.js
+++ contentScript.js
@@ -1,24 +1,43 @@
 function scrollAndCaptureHTML(callback) {
-    let scrolls = 0;
     let lastScrollHeight = 0;
-  
-    const intervalId = setInterval(() => {
-      console.log(`Scrolling ${scrolls} times...`);
+    let attempts = 0;
+    let countOfLoad = 0;
+    const maxAttempts = 10; // 최대 시도 횟수를 정의하여 무한 스크롤을 방지합니다.
 
-      window.scrollTo(0, document.body.scrollHeight);
-      const currentScrollHeight = document.body.scrollHeight;
-  
-      // 스크롤이 더 이상 변화가 없다면, 끝난 것으로 간주
-      if (lastScrollHeight === currentScrollHeight) {
-        console.log("Scrolling finished.");
-        clearInterval(intervalId);
-        callback(document.documentElement.outerHTML);
-    } else {
-        lastScrollHeight = currentScrollHeight;
-        scrolls++;
-      }
-    }, 5000);
-  }
+    // 변화 감지를 위한 observer 생성
+    const observer = new MutationObserver((mutations, obs) => {
+        const currentScrollHeight = document.body.scrollHeight;
+
+        if (lastScrollHeight === currentScrollHeight && attempts < maxAttempts) {
+            attempts++;
+            console.log(`Scrolling attempt ${attempts}/${maxAttempts}.`);
+            setTimeout(() => window.scrollTo(0, document.body.scrollHeight), 1000);
+        } else if (attempts >= maxAttempts) {
+            // 최대 시도 횟수에 도달했거나 더 이상 콘텐츠가 로드되지 않을 경우
+            console.log("Scrolling finished or max attempts reached.");
+            obs.disconnect(); // Observer를 중지합니다.
+            callback(document.documentElement.outerHTML); // 콜백 함수 호출
+        } else {
+            countOfLoad++;
+            console.log('countOfLoad: ' + countOfLoad);
+
+            lastScrollHeight = currentScrollHeight;
+            attempts = 0; // 시도 횟수를 초기화하고 계속 스크롤합니다.
+            setTimeout(() => window.scrollTo(0, document.body.scrollHeight), 1000);
+        }
+    });
+
+    // 문서 전체에 대한 변화 감지 시작
+    observer.observe(document, {
+        childList: true,
+        subtree: true
+    });
+
+    console.log('observer started');
+
+    // 초기 스크롤 시작
+    window.scrollTo(0, document.body.scrollHeight);
+}
   
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "scrollAndCapture") {
